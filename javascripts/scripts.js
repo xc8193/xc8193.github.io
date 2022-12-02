cf15e2ee453c238a129abb5606811318e60bf24b
xc8193 <118708484+xc8193@users.noreply.github.com>
2022-12-02 16:35:39 -0500

    finalproj
    final proj

---
+++ javascripts/scripts.js
@@ -0,0 +1,45 @@
+
+// I WANTED TO MOVE THE BIRD ON CLICK BUT IT MOVES EVERYTHING ON THE SCREEN
+// document.addEventListener('click', function handleClick(event) {
+// event.target.classList.add('test');
+//
+// });
+
+// var image = document.getElementById('BIRD');
+
+
+// document.addEventListener('click', function handleClick(event) {
+//   event.target.classList.add('test');
+// });
+
+function handleClick(element) {
+  event.target.classList.add("test");
+
+}
+
+
+
+
+// document.addEventListener('click', function handleClick(event) {
+//   var x = 0;
+//   if (x == 0) {
+// event.target.classList.add('test');
+//     x = 1;
+//   } else {
+//     event.target.classList.remove('test');
+//     x = 0;
+//
+// });
+//
+//
+//
+//
+// document.addEventListener ("click",function handleClick(event) {
+//   if (x == 0) {
+//     document.getElementById("click").classList.add("test");
+//     x = 1;
+//   } else {
+//     document.getElementById("click").classList.remove("test");
+//     x = 0;
+//   }
+// }
